55f0887882e701dfc7aa09f576f1cafbb90ed3d4
sahaj9897 <sahaj9897@gmail.com>
2025-06-09 23:57:31 +0530

    added all model and css in project

---
+++ frontend/src/main.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+
+import {
+  RouterProvider,
+} from "react-router-dom";
+import {router} from './Routes'
+import './index.css'
+import { AuthProvider } from './store/Auth';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <AuthProvider>
+  <React.StrictMode>
+   <RouterProvider router={router} />
+  </React.StrictMode>
+  </AuthProvider>
+)
