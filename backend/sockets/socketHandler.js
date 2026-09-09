a16cee7025e59aae7eb61d8ce427fcaf0e0f066a
sahaj9897 <sahaj9897@gmail.com>
2026-09-09 20:11:33 +0530

    bug fixed

--- backend/sockets/socketHandler.js
+++ backend/sockets/socketHandler.js
@@ -5,7 +5,7 @@ const setupSocket = (server) => {
 
   const io = socket(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: process.env.FRONTEND_URL || "http://localhost:5173",
       credentials: true,
     },
   });
